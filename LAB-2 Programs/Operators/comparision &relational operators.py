1ba208ac585ea6e4e2bca195d389b69706321aec
SAI-551 <vallurisai874@gmail.com>
2026-08-04 22:11:28 +0530

    Delete LAB-2 Programs directory

--- LAB-2 Programs/Operators/comparision &relational operators.py
+++
@@ -1,9 +0,0 @@
-Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> m = int(input("Enter m: "))
-... n = int(input("Enter n: "))
-... print("m == n :", m == n)
-... print("m != n :", m != n)
-... print("m > n  :", m > n)
-... print("m < n  :", m < n)
-... print("m >= n :", m >= n)
